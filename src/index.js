c56e2e8ee9e76f5e918dc44c56cb2bde7e916970
LeonelMadrid13 <leoneleduardomadrid@hotmail.com>
2023-09-01 02:16:31 -0600

    Created src folder

---
+++ src/index.js
@@ -0,0 +1,1 @@
+// principal backend file
---
+++ src/server.js
@@ -0,0 +1,14 @@
+//create a simple express server app
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    }
+);
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+    }
+);
